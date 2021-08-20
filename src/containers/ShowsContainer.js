46b9bd43bd8c22601cdbe6d12e2f2aa85040d00c
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-20 16:10:20 -0400

    Edit site title and set up Show components

---
+++ src/containers/ShowsContainer.js
@@ -0,0 +1,12 @@
+import Show from '../components/Show';
+
+function ShowsContainer() {
+    return(
+        <div id="shows-container">
+            <h1>Shows Container</h1>
+            <Show />
+        </div>
+    )
+}
+
+export default ShowsContainer;
