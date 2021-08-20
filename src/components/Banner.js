2d50897ab5d2aa1b2ca1bf9f8be0cb10a84f524b
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-20 19:16:16 -0400

    Add banner image and basic styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import bannerImage from '../banner.jpg';
 
 function Banner() {
     return(
-        <div id="banner">
-            <span>Banner</span>
+        <div id="banner" style={{ backgroundImage: `url(${bannerImage})`, backgroundSize: "cover" }}>
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,8 @@ function Footer() {
             <h5>Company</h5>
             <h5>Work with Us</h5>
             <h5>Connect</h5>
+            <br />
+            <span>Banner Credit: Photo by <a href="https://unsplash.com/@alevisionco?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">alevision.co</a> on <a href="https://unsplash.com/s/photos/theater?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>
         </div>
     );
 }
