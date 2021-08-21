addc2c7ae112fa3b11296ee0a838f0fcb070cd86
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-20 23:15:13 -0400

    Add NavLinks to ShowList component

--- src/components/ShowsList.js
+++ src/components/ShowsList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const ShowsList = props => {
     return(
         <div id="shows-list">
             <ul>
-                {props.shows.map(show => <li key={show.id}>{show.title}</li>)}
+                {props.shows.map(show => <li key={show.id}><NavLink to={`shows/${show.id}`}>{show.title}</NavLink></li>)}
             </ul>
         </div>
     )
