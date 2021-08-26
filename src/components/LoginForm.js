0b015da5c87fab5e91c4e8e954673b90fa66eb6a
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-25 20:59:30 -0400

    Add simple styling to login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
     render() {
         return(
-            "Login"
+            <div id="login-form">
+                <h3>Sign in to Goodtheater</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label for="email">Email address</label>
+                    <input type="text" id="email" />
+                    <br />
+                    <label for="password">Password</label>
+                    <input type="password" />
+                    <br />
+                    <input type="submit" value="Sign in" />
+                </form>
+            </div>
         )
     }
 }
