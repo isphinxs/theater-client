8e080bb954b38271cc29bec6156aab6f8351edd4
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-25 21:14:17 -0400

    Control login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+    state = {
+        email: "",
+        password: ""
+    }
+
+    handleEmailChange = event => {
+        this.setState({ email: event.target.value });
+    }
+
+    handlePasswordChange = event => {
+        this.setState({ password: event.target.value });
+    }
+
     handleSubmit = event => {
         event.preventDefault();
+        // debugger;
     }
 
     render() {
@@ -11,10 +25,20 @@ class LoginForm extends Component {
                 <h3>Sign in to Goodtheater</h3>
                 <form onSubmit={this.handleSubmit}>
                     <label for="email">Email address</label>
-                    <input type="text" id="email" />
+                    <input 
+                        type="text" 
+                        id="email" 
+                        onChange={event => this.handleEmailChange(event)}
+                        value={this.state.email}
+                    />
                     <br />
                     <label for="password">Password</label>
-                    <input type="password" />
+                    <input 
+                        type="password" 
+                        id="password"
+                        onChange={event => this.handlePasswordChange(event)}
+                        value={this.state.password}
+                    />
                     <br />
                     <input type="submit" value="Sign in" />
                 </form>
