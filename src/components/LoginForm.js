5458f397032db086d1c8665bcb1bedbd97a4700c
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-25 21:32:01 -0400

    Convert class to functional component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,50 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class LoginForm extends Component {
-    state = {
-        email: "",
-        password: ""
+function LoginForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+ 
+    const handleEmailChange = event => {
+        setEmail(event.target.value);
     }
 
-    handleEmailChange = event => {
-        this.setState({ email: event.target.value });
+    const handlePasswordChange = event => {
+        setPassword(event.target.value);
     }
 
-    handlePasswordChange = event => {
-        this.setState({ password: event.target.value });
-    }
-
-    handleSubmit = event => {
+    const handleSubmit = event => {
         event.preventDefault();
-        // debugger;
+        debugger;
     }
 
-    render() {
-        return(
-            <div id="login-form">
-                <h3>Sign in to Goodtheater</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <label for="email">Email address</label>
-                    <input 
-                        type="text" 
-                        id="email" 
-                        onChange={event => this.handleEmailChange(event)}
-                        value={this.state.email}
-                    />
-                    <br />
-                    <label for="password">Password</label>
-                    <input 
-                        type="password" 
-                        id="password"
-                        onChange={event => this.handlePasswordChange(event)}
-                        value={this.state.password}
-                    />
-                    <br />
-                    <input type="submit" value="Sign in" />
-                </form>
-            </div>
-        )
-    }
+    
+    return(
+        <div id="login-form">
+            <h3>Sign in to Goodtheater</h3>
+            <form onSubmit={handleSubmit}>
+                <label for="email">Email address</label>
+                <input 
+                    type="text" 
+                    id="email" 
+                    onChange={event => handleEmailChange(event)}
+                    value={email}
+                />
+                <br />
+                <label for="password">Password</label>
+                <input 
+                    type="password" 
+                    id="password"
+                    onChange={event => handlePasswordChange(event)}
+                    value={password}
+                />
+                <br />
+                <input type="submit" value="Sign in" />
+            </form>
+        </div>
+    )
 }
 
 export default LoginForm;
