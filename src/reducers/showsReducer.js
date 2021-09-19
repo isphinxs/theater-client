2e59c002c26001eba7a08decea6d38ebbf37644a
Samantha Ostrowski <isphinxs@hotmail.com>
2021-09-18 20:53:56 -0400

    Add Heroku URL

--- src/reducers/showsReducer.js
+++ src/reducers/showsReducer.js
@@ -1,3 +1,6 @@
+const URL = 'https://rocky-mountain-05852.herokuapp.com';
+// const URL = 'http://localhost:3000';
+
 const initialState = {
     shows: [],
     savedShows: [],
@@ -102,7 +105,7 @@ function showsReducer(state = initialState, action) {
 
 export function fetchShows(dispatch) {
     dispatch({ type: 'shows/loading' });
-    fetch('http://localhost:3000/shows')
+    fetch(`${URL}/shows`)
         .then(resp => resp.json())
         .then(data => dispatch({
                 type: 'shows/setShows',
@@ -116,7 +119,7 @@ export function fetchShows(dispatch) {
 
 export function checkForSavedShows(dispatch, getState) {
     dispatch({ type: 'shows/loading' });
-    fetch(`http://localhost:3000/users/${getState().user.id}`, {
+    fetch(`${URL}/users/${getState().user.id}`, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`
         }
@@ -150,7 +153,7 @@ export function saveShow(show) {
         }
 
         try {
-            const resp = await fetch(`http://localhost:3000/users/${getState().user.id}`, {
+            const resp = await fetch(`${URL}/users/${getState().user.id}`, {
                 method: 'PATCH',
                 headers: {
                     'content-type': 'application/json',
@@ -184,7 +187,7 @@ export function removeShow(show) {
         }
         
         try {
-            const resp = await fetch(`http://localhost:3000/users/${getState().user.id}`, {
+            const resp = await fetch(`${URL}/users/${getState().user.id}`, {
                 method: 'PATCH',
                 headers: {
                     'content-type': 'application/json',
@@ -209,7 +212,7 @@ export function createUser(name, email, password) {
     return async function createUserThunk(dispatch) {
         dispatch({ type: 'user/authenticating' });
         try {
-            fetch('http://localhost:3000/users', {
+            fetch(`${URL}/users`, {
                 method: 'POST',
                 headers: {
                     'content-type': 'application/json',
@@ -236,7 +239,7 @@ export function saveUser(email, password) {
     return async function saveUserThunk(dispatch) {
         dispatch({ type: 'user/authenticating' });
         try {
-            fetch('http://localhost:3000/api/v1/auth', {
+            fetch(`${URL}/api/v1/auth`, {
                 method: 'POST',
                 headers: {
                     'content-type': 'application/json',
