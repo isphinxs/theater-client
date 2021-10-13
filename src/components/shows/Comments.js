b8d05bebec0e55fd69680c956eb1ac7c2fb790de
Samantha Ostrowski <isphinxs@hotmail.com>
2021-10-12 22:25:00 -0400

    Draft Comments and Ratings components

---
+++ src/components/shows/Comments.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Comments() {
+    return(
+        <h3>Comments</h3>
+    )
+}
+
+export default Comments;
---
+++ src/components/shows/Ratings.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Ratings() {
+    return(
+        <h3>Ratings</h3>
+    )
+}
+
+export default Ratings;
