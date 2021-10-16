4fee77618290c5a6641fc6d7f7c8acdd7226b235
Samantha Ostrowski <isphinxs@hotmail.com>
2021-10-16 16:14:00 -0400

    Remove ShowView placeholder text

--- src/components/shows/ShowView.js
+++ src/components/shows/ShowView.js
@@ -23,7 +23,6 @@ const ShowView = props => {
 
     return(
         <React.Fragment>
-            <h3>Show View</h3>
             <Show show={props.show} handleClick={props.handleClick} isLoggedIn={props.isLoggedIn} />
             <button className="gr-button" onClick={handleClick}>Add to Saved</button>
             <button className="gr-button" onClick={handleClick}>Edit</button>
